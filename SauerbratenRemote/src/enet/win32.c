2c7b6e6530e04fdcad1950d3a81a9d3fccf499e8
furroy <furroy@gmail.com>
2008-07-28 22:11:15 -0500

    added inet_aton() func to windows version

--- SauerbratenRemote/src/enet/win32.c
+++ SauerbratenRemote/src/enet/win32.c
@@ -53,6 +53,105 @@ enet_time_set (enet_uint32 newTimeBase)
 }
 
 #ifdef TC
+
+int
+inet_aton(const char *cp, struct in_addr * addr)
+	
+
+{
+	unsigned int	val;
+	int		base,
+			n;
+	char		c;
+	u_int		parts[4];
+	u_int	   *pp = parts;
+
+	for (;;)
+	{
+		/*
+		 * Collect number up to ``.''. Values are specified as for C:
+		 * 0x=hex, 0=octal, other=decimal.
+		 */
+		val = 0;
+		base = 10;
+		if (*cp == '0')
+		{
+			if (*++cp == 'x' || *cp == 'X')
+				base = 16, cp++;
+			else
+				base = 8;
+		}
+		while ((c = *cp) != '\0')
+		{
+			if (isdigit((unsigned char) c))
+			{
+				val = (val * base) + (c - '0');
+				cp++;
+				continue;
+			}
+			if (base == 16 && isxdigit((unsigned char) c))
+			{
+				val = (val << 4) +
+					(c + 10 - (islower((unsigned char) c) ? 'a' : 'A'));
+				cp++;
+				continue;
+			}
+			break;
+		}
+		if (*cp == '.')
+		{
+			/*
+			 * Internet format: a.b.c.d a.b.c	(with c treated as
+			 * 16-bits) a.b		(with b treated as 24 bits)
+			 */
+			if (pp >= parts + 3 || val > 0xff)
+				return 0;
+			*pp++ = val, cp++;
+		}
+		else
+			break;
+	}
+
+	/*
+	 * Check for trailing junk.
+	 */
+	while (*cp)
+		if (!isspace((unsigned char) *cp++))
+			return 0;
+
+	/*
+	 * Concoct the address according to the number of parts specified.
+	 */
+	n = pp - parts + 1;
+	switch (n)
+	{
+
+		case 1:			/* a -- 32 bits */
+			break;
+
+		case 2:			/* a.b -- 8.24 bits */
+			if (val > 0xffffff)
+				return 0;
+			val |= parts[0] << 24;
+			break;
+
+		case 3:			/* a.b.c -- 8.8.16 bits */
+			if (val > 0xffff)
+				return 0;
+			val |= (parts[0] << 24) | (parts[1] << 16);
+			break;
+
+		case 4:			/* a.b.c.d -- 8.8.8.8 bits */
+			if (val > 0xff)
+				return 0;
+			val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
+			break;
+	}
+	if (addr)
+		addr->s_addr = htonl(val);
+	return 1;
+}
+
 void
 enet_address_set_host_ip(ENetAddress * address, const char * name)
 {
