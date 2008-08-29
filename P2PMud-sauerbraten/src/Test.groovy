7bf6d9edc953d6187b082431575a4549f48abfd2
William Burdick <bill@sheba.(none)>
2008-08-28 21:15:00 -0500

    file filter fix

--- P2PMud-sauerbraten/src/Test.groovy
+++ P2PMud-sauerbraten/src/Test.groovy
@@ -285,7 +285,7 @@ public class Test {
 		def ch = new JFileChooser();
 		ch.setDialogTitle("Please choose the DF map to load");
 		ch.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES)
-		
+
 //		def filter = {
 //                  accept: {
 //					dir, 
@@ -294,7 +294,7 @@ public class Test {
 //                  getDescription: { "DF ASCII Maps (*.txt)" }
 //		} as FileFilter
                   
-		ch.setFileFilter(new GroovyFileFilter("DF ASCII MAPS (*.txt)") {it.name.toLowerCase().endsWith(".txt")})
+		ch.setFileFilter(new GroovyFileFilter("DF ASCII MAPS (*.txt)") {it.isDirectory() || it.name.toLowerCase().endsWith(".txt")})
 		if (ch.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 			Thread.start {
 				if (ch.getSelectedFile().isFile()) {
