ac09fab3335e8372cba9c9d1e8749d195e561af2
William Burdick <bill@sheba.(none)>
2008-09-06 21:14:47 -0500

    gui & error msg tweaks

--- P2PMud-sauerbraten/src/Plexus.groovy
+++ P2PMud-sauerbraten/src/Plexus.groovy
@@ -200,14 +200,16 @@ public class Plexus {
 					}
 					label(text: "Command: ")
 					fields.cmd = textField(actionPerformed: {cmd()}, constraints: 'wrap, growx')
-					label(text: 'Generation')
-					panel(layout: new MigLayout('fill, ins 0'), constraints: 'growx,wrap') {
-						button(text: "Launch 3D", actionPerformed: {launchSauer()})
-						button(text: "Generate Dungeon", actionPerformed: {generateDungeon()})
-						button(text: "Load DF Map", actionPerformed: {loadDFMap()})
-						panel(constraints: 'growx,wrap')
-					}
 					tabbedPane(constraints: 'spanx,width 100%,growy,wrap') {
+						label(text: 'Generation')
+						panel(name: 'Commands', layout: new MigLayout('fill')) {
+							panel(layout: new MigLayout('fill, ins 0'), constraints: 'growx,wrap') {
+								button(text: "Launch 3D", actionPerformed: {launchSauer()})
+								button(text: "Generate Dungeon", actionPerformed: {generateDungeon()})
+								button(text: "Load DF Map", actionPerformed: {loadDFMap()})
+								panel(constraints: 'growx,wrap')
+							}
+						}
 						panel(name: 'Stats', layout: new MigLayout('fill')) {
 							field('x: ', 'x')
 							field('y: ', 'y')
@@ -227,9 +229,6 @@ public class Plexus {
 							field('physics state: ', 'ps')
 							field('max speed: ', 'ms')
 							panel(constraints: 'growy,wrap')
-						}
-						panel(name: 'Commands', layout: new MigLayout('fill')) {
-							
 						}
 					}
 				}
